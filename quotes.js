da871f5eb161a071e4444a9387c9aa0245d0e87c
Tamara Batieva <batieva.tamara@gmail.com>
2016-05-06 18:23:31 +0300

    Centre the box, add button, new file with quotes

---
+++ quotes.js
@@ -0,0 +1,7 @@
+var quotes = {" David Allen": "You can do anything, but not everything",
+    "Antoine de Saint-Exupery": "Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.",
+    "Unknown Author": "The richest man is not he who has the most, but he who needs the least.",
+    "Wayne Gretzky": "You miss 100 percent of the shots you never take.",
+    "Ambrose Redmoon": "Courage is not the absence of fear, but rather the judgement that something else is more important than fear.",
+    "Lao-Tze": "Watch your thoughts; they become words. Watch your words; they become actions. Watch your actions; they become habits. Watch your habits; they become character. Watch your character; it becomes your destiny."
+}
